84d810d885a7473f6b7066fc66b305ab7d77bc0d
deckman <cosstropolis@gmail.com>
2014-01-29 02:23:10 -0500

    can now switch keys

--- src/Instrument.c
+++ src/Instrument.c
@@ -12,12 +12,31 @@ const u16 PITCHES[] = {0,
 };
 const u16 LEN_PITCHES = sizeof(PITCHES) / sizeof(PITCHES[0]);
 const u16 SCALE_MAJ[] = {0,2,4,5,7,9,11,12};
+const u16 SCALES[9][SCALE_LENGTH] = {
+	{0,2,4,5,7,9,11,12},
+	{0,2,3,5,7,9,10,12},
+	{0,1,3,5,7,9,10,12},
+	{0,2,4,6,7,9,11,12},
+	{0,2,4,5,7,9,10,12},
+	{0,2,3,5,7,8,10,12},
+	{0,1,3,5,6,8,10,12},
+	{0,2,3,5,7,9,11,12}, //melodic minor
+	{0,2,3,5,7,8,11,12}, //harmonic minor
+};
 u16 currentPitchIndex[4] = {0,0,0,0};
 u8 octave[4] = {4,4,4,4};
 u8 pitchModDepth[4] = {1,1,1,1};
 u8 envelope[4] = {0,0,0,0};
 u8 sustainOn[4] = {0,0,0,0};
-u16 scale[4][8];
+u16 scale[4][SCALE_LENGTH];
+u8 keyIndex[2];
+u8 tonicList[2][MAX_KEYS] = {
+	{C}
+};
+u8 modeList[2][MAX_KEYS] = {
+	{MAJOR,MINOR,MINOR_MEL,MINOR_HARM},
+	{MAJOR,MINOR,MINOR_MEL,MINOR_HARM}
+};
 u8 vibratoOn[4] = {0,0,0,0};
 s16 vibratoY[4] = {0,0,0,0};
 u16 vibratoX[4] = {0,0,0,0};
@@ -26,11 +45,13 @@ u16 vibratoSpeed[4] = {60,80,80,80};
 
 void Instrument_init(){
 	int i,j;
-	for(i=0; i<3; i++){
-		//PSG_setEnvelope(i, envelope[i]);
-		for (j=0; j<8; j++){
-			scale[i][j] = SCALE_MAJ[j];
-		}
+	for(i=0; i<2; i++){
+		//replace with setKey()
+		setKey(i, 0);
+		//for (j=0; j<8; j++){
+		//	scale[i][j] = SCALE_MAJ[j];
+		//}
+
 	}
 }
 
@@ -70,9 +91,9 @@ void Instrument_joyEvent(u16 joy, u16 changed, u16 state){
 		//Zs: key shift up
 
 		if (BUTTON_X & state & changed){
-			//if start is pressed, change vibrato
+			//if start is pressed, go to previous key
 			if (BUTTON_START & state)
-				vibratoOn[channel] = ! (vibratoOn[channel]);
+				setKey(joy, clamp((keyIndex[joy]-1), 0, MAX_KEYS));
 			//else, decrease octave
 			else octave[channel] = clamp(octave[channel]-1, OCTAVE_MIN, OCTAVE_MAX);
 		}
@@ -84,9 +105,11 @@ void Instrument_joyEvent(u16 joy, u16 changed, u16 state){
 			else sustainOn[channel] = ! (sustainOn[channel]);
 		}
 		if (BUTTON_Z & state & changed){
-			if (BUTTON_START & state);
+			if (BUTTON_START & state)
+				setKey(joy, (keyIndex[joy]+1) % MAX_KEYS);
 			else octave[channel] = clamp(octave[channel]+1, OCTAVE_MIN, OCTAVE_MAX);
 		}
+
 		if (BUTTON_UP & state){
 			pitchMod = pitchModDepth[channel];
 		}
@@ -94,9 +117,13 @@ void Instrument_joyEvent(u16 joy, u16 changed, u16 state){
 			pitchMod = -pitchModDepth[channel];
 		}
 
+		if (bA & changed && BUTTON_START & state){
+			vibratoOn[channel] = ! (vibratoOn[channel]);
+		}
+		else if (bC & changed && BUTTON_START & state);
 		//play
 		//if any button is pressed and right is held, note will play
-		if (BUTTON_RIGHT & state){
+		else if (BUTTON_RIGHT & state){
 			envelope[channel] = ENV_DEFAULT;
 			setCPI(channel, scale[channel][buttonsToScalePitch(bA, bB, bC)], pitchMod);
 		}
@@ -161,6 +188,18 @@ static void updateVibrato(u8 i){
 	else{
 		vibratoY[i] = 0;
 	}
+}
+
+static void setKey(u8 joy, u8 ki){
+	int i,j;
+	keyIndex[joy] = ki;
+	for (j=0;j<2;j++){
+		for (i=0;i<SCALE_LENGTH;i++){
+			//0,1 if joy0; 2,3 if joy1
+			scale[j + (joy * 2)][i] = SCALES[modeList[joy][ki]][i];
+		}	
+	}
+
 }
 
 static u32 clamp(u32 n, u32 min, u32 max){
--- src/Instrument.h
+++ src/Instrument.h
@@ -2,7 +2,7 @@
 #define _INSTRUMENT_H_
 
 #include <genesis.h>
-//we need to include HUD so we only do the draw call when we need to
+// we need to include HUD so we only do the draw call when we need to
 #include "HUD.h"
 
 #define OCT 12
@@ -12,11 +12,44 @@
 #define ENV_DEFAULT 3
 #define CHANNEL_DEF_JOY0 0
 #define CHANNEL_DEF_JOY1 2
+#define MAX_KEYS 10
+#define SCALE_LENGTH 8
 
+// modes
+#define MAJOR 0
+#define DORIAN 1
+#define PHRYGIAN 2
+#define LYDIAN 3
+#define MIXOLYDIAN 4
+#define MINOR 5
+#define LOCRIAN 6
+#define MINOR_MEL 7
+#define MINOR_HARM 8
+// keys
+#define C 0
+#define C_S 1
+#define D 2
+#define D_S 3
+#define E 4
+#define F 5
+#define F_S 6
+#define G 7
+#define G_S 8
+#define A 9
+#define A_S 10
+#define B 11
+
+// channel settings
 extern u8 octave[4];
 extern u8 sustainOn[4];
 extern u8 vibratoOn[4];
 extern u8 pitchModDepth[4];
+//extern u8 tonic[4];
+extern u16 scale[4][8];
+// player settings
+extern u8 keyIndex[2];
+extern u8 tonicList[2][MAX_KEYS];
+extern u8 modeList[2][MAX_KEYS];
 
 void Instrument_init();
 void Instrument_update();
@@ -28,5 +61,6 @@ static u8 buttonsToScalePitch(u8, u8, u8);
 static void setCPI(u8 channel, u8 scalePitch, s8 pitchMod);
 static void updateVibrato(u8 channel);
 static u32 clamp(u32 n, u32 min, u32 max);
+static void setKey(u8 joy, u8 ki);
 
 #endif /* _INSTRUMENT_H_ */
