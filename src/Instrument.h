6e85d07e19dcac650c379c9154e761f50c0988b4
deckman <cosstropolis@gmail.com>
2015-03-27 15:11:09 -0400

    basic noise support

--- src/Instrument.h
+++ src/Instrument.h
@@ -11,15 +11,17 @@
 #define VIB_SPEED_DEFAULT 80
 #define VIB_X_MAX 511
 #define PORTA_X_MAX 511
-#define ENV_DEFAULT 3
+#define ENV_DEFAULT 5
 #define SUS_OFF 0
 #define SUS_STRUM 1
 #define SUS_HAMMER 2
 #define CHANNEL_DEF_JOY0 0
 #define CHANNEL_DEF_HARMONY 1
+#define CHANNEL_DEF_NOISE 3
 #define CHANNEL_DEF_JOY1 2
 #define MAX_KEYS 10
 #define SCALE_LENGTH 8
+#define MAX_NOISE_MODES 5
 
 // modes
 #define MAJOR 0
@@ -65,6 +67,9 @@ extern u8 modeList[2][MAX_KEYS];
 extern u8 paused[2];
 extern u8 playing[2];
 extern u8 harmonyOn;
+extern u8 noiseOn;
+// extern u8 noiseType;
+// extern u8 noiseFreq;
 
 extern u16 portamentoX[4];
 extern s32 portamentoY[4];
@@ -75,6 +80,7 @@ void Instrument_joyEvent(u16 joy, u16 changed, u16 state);
 void Instrument_playNote(u8 channel, u8 envelope);
 void Instrument_stopNote(u8 channel);
 void Instrument_setPitch(u8 joy, u8 scalePitch, u8 pitchMod);
+// void Instrument_setNoise(u8 joy, u8 scalePitch, u8 pitchMod);
 u8 Instrument_buttonsToScalePitch(u8, u8, u8);
 
 #endif /* _INSTRUMENT_H_ */
