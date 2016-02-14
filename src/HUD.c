4a432ecdc18c05b526de158f8ea33f06e498b728
decky <coss@cosstropolis.com>
2016-02-14 13:57:19 -0700

    fixed column alignment; added blank pause menu

--- src/HUD.c
+++ src/HUD.c
@@ -1,11 +1,18 @@
 #include "HUD.h"
 
+#define leftEdge(col) (col * ((WIDTH_TILES-(HORIZONTAL_MARGIN*2)) / 2)) + (HORIZONTAL_MARGIN)
+
 /* clear area from c to c+w and draw text over it
  * (c is relative to x)
  */ 
 static void drawText(char *text, u16 x, u16 y, u16 c, u16 w);
 
 static void drawStatusMsg(u8 joy, u8 line);
+
+static void createHUD(u8 player);
+static void createHUDAll();
+static void clearPlayerSide(u8 player);
+static void createPauseMenu(u8 player);
 
 const u32 circle[8]=
 {
@@ -50,6 +57,19 @@ const u32 note[16]=
 	0x00000000
 };
 
+const u32 overscore[8]=
+{
+	0xFFFFFFFF,
+	// 0xFFFFFFFF,
+	0x00000000,
+	0x00000000,
+	0x00000000,
+	0x00000000,
+	0x00000000,
+	0x00000000,
+	0x00000000,
+};
+
 const u8 statusCodes[9] = {
 	STATUS_CODE_KEY,
 	STATUS_CODE_KEY, //store key twice, because it's written on two lines
@@ -65,41 +85,12 @@ const u8 statusCodes[9] = {
 u8 test = 0;
 
 void HUD_init(){
-	u16 i, x, y;
-
 	VDP_loadTileData((const u32 *)circle, TILE_USERINDEX, 1, 0);
 	VDP_loadTileData((const u32 *)circlePressed, TILE_USERINDEX+1, 1, 0);
 	VDP_loadTileData((const u32 *)note, TILE_USERINDEX+2, 2, 0);
+	VDP_loadTileData((const u32 *)overscore, TILE_USERINDEX+4, 2, 0);
 
-	for (i=0; i<2; i++){
-		x = X0 + (i * (W_TILES/2));
-		if (i) x--;
-		y = Y_STATUS;
-
-		VDP_drawText("Tonic", x, y);
-		VDP_drawText("Mode", x, y+1);
-		VDP_drawText("Octave", x, y+2);
-		VDP_drawText("Sustain", x, y+3);
-		//hard-coded blank line - check Y_STATUS_BLANK_LINE if this messes things up
-		y++;
-		VDP_drawText("Mod depth", x, y+4);
-		VDP_drawText("Vibrato", x, y+5);
-		VDP_drawText("Arpeggio", x, y+6);
-		if (!i) VDP_drawText("Harmony", x, y+7);
-		else VDP_drawText("Noise", x, y+7);
-		VDP_drawText("Portamento", x, y+8);
-
-		if (!i){
-			VDP_drawText("PLAYER  1", x+X_PLAYER_NAME, y+11);
-		}
-		else{
-			VDP_drawText("PLAYER  2", x+X_PLAYER_NAME, y+11);
-		}
-
-		HUD_updateStatusView(i, 0, 0, 0, 0, 255);
-	}
-	VDP_drawText("v0.2dev",32,25);
-	VDP_drawText("COSSTROPOLIS.COM",23,26);
+	createHUDAll();
 
 	// VDP_setTileMap(APLAN, 5, 24);
 
@@ -111,14 +102,98 @@ void HUD_init(){
 	// 	VDP_drawText(text, 1 + (i/8 * 10), 17+ (i%8));
 	// }
 }
+
+static void clearPlayerSide(u8 player){
+
+	u8 i = 0;
+	u8 x = (WIDTH_TILES/2) * player;
+
+	char blankString[(WIDTH_TILES/2)+1];
+
+	for (i=0; i<WIDTH_TILES/2; i++){
+		blankString[i] = ' ';
+	}
+	blankString[WIDTH_TILES/2] = '\0';
+
+	for (i=0; i<HEIGHT_TILES; i++){
+		VDP_drawText(blankString, x, i);
+	}
+}
+
+static void createHUD(u8 player){
+	u16 i, x, y;
+
+	// for (i=0; i<2; i++){
+	// x = X0 + (player * (WIDTH_TILES/2));
+	// if (player) x--;
+	x = leftEdge(player);
+	y = Y_STATUS;
+
+	VDP_drawText("Tonic", x, y);
+	VDP_drawText("Mode", x, y+1);
+	VDP_drawText("Octave", x, y+2);
+	VDP_drawText("Sustain", x, y+3);
+	//hard-coded blank line - check Y_STATUS_BLANK_LINE if this messes things up
+	y++;
+	VDP_drawText("Mod depth", x, y+4);
+	VDP_drawText("Vibrato", x, y+5);
+	VDP_drawText("Arpeggio", x, y+6);
+	if (!player) VDP_drawText("Harmony", x, y+7);
+	else VDP_drawText("Noise", x, y+7);
+	VDP_drawText("Portamento", x, y+8);
+
+	if (!player){
+		VDP_drawText("PLAYER  1", x+X_PLAYER_NAME, y+11);
+	}
+	else{
+		VDP_drawText("PLAYER  2", x+X_PLAYER_NAME, y+11);
+	}
+
+	HUD_updateStatusView(player, 0, 0, 0, 0, 255);
+	// }
+
+	if (player){
+		VDP_drawText("v0.2dev",32,25);
+		VDP_drawText("COSSTROPOLIS.COM",23,26);
+	}
+}
+
+static void createPauseMenu(u8 player){
+
+	u8 x = leftEdge(player), y = Y_PAUSE;
+	u8 i;
+
+	VDP_drawText("      OPTIONS", x, y);
+	//draw underline
+	for (i=x+6; i<x+13; i++){
+		VDP_setTileMapXY(APLAN, TILE_ATTR_FULL(PAL0, 0, 0, 0, TILE_USERINDEX+4), i, y+1);
+	}
+
+	VDP_drawText("Coming soon...", x, y+2);
+	VDP_drawText("Double-tap START", x, y+5);
+	VDP_drawText("to resume", x, y+6);
+}
+
+static void createHUDAll(){
+	u8 i;
+	for (i=0; i<2; i++){
+		createHUD(i);
+	}
+}
+
 void HUD_update(){
 
-	// char text[5];
-	// uintToStr(currentPitchIndex[0], text, 5);
-	// VDP_drawText(text, 10, 21);
+}
 
-	// intToStr(portamentoY[0], text, 5);
-	// VDP_drawText(text, 10, 22);
+void HUD_pauseSet(u8 joy, u8 pauseState){
+	clearPlayerSide(joy);
+
+	if (! pauseState){
+		createHUD(joy);
+	}
+	else {
+		createPauseMenu(joy);
+	}
 }
 
 void HUD_joyEvent(u16 joy, u16 changed, u16 state){
@@ -143,11 +218,13 @@ void HUD_updateStatusView(u8 joy, u8 bA, u8 bB, u8 bC, u8 bStart, u8 statusCode)
 		}
 	}
 
-	u16 x = X0 + (joy * (W_TILES/2));
-	if (joy) x--;
+	// u16 x = X0 + (joy * (WIDTH_TILES/2));
+	// if (joy) x--;
+	// u16 x = (joy * ((WIDTH_TILES-(HORIZONTAL_MARGIN*2)) / 2)) + (HORIZONTAL_MARGIN);
+	u16 x = leftEdge(joy);
 	u16 y = Y_STATUS;
 
-	if (bStart){
+	if (bStart && statusCode){
 		bA = bB = bC = 0;
 	}
 	else{
@@ -195,6 +272,7 @@ void HUD_updateStatusView(u8 joy, u8 bA, u8 bB, u8 bC, u8 bStart, u8 statusCode)
 
 	// VDP_fillTileMapRectInc(APLAN, TILE_ATTR_FULL(PAL0, 0, 0, 0, TILE_USERINDEX+2), x+X_NOTE, 25, 1, 2);
 
+	//draw musical note
 	if (playing[joy]){
 		VDP_fillTileMapRectInc(APLAN, TILE_ATTR_FULL(PAL0, 0, 0, 0, TILE_USERINDEX+2), x+X_NOTE, Y_NOTE, 1, 2);
 	}
@@ -262,12 +340,16 @@ static void drawStatusMsg(u8 joy, u8 line){
 			}
 			else{
 				// strcpy(text, (noiseOn) ? "ON" : "OFF");
-				if (noiseOn){
-					uintToStr(noiseOn, text, 1);
-				}
-				else{
-					strcpy(text, "OFF");
-				}
+				// if (noiseOn){
+				// 	uintToStr(noiseOn, text, 1);
+				// }
+				// else{
+				// 	strcpy(text, "OFF");
+				// }
+				strcpy(text, (noiseOn==NOISE_MODE_CHAN3PERIOD) ? "PERIOD" :
+					(noiseOn==NOISE_MODE_CHAN3WHITE) ? "WHITE" :
+					(noiseOn==NOISE_MODE_CLOCKWHITE) ? "CLOCK" :
+					"OFF");
 			}
 			break;
 		case 8:
@@ -285,5 +367,9 @@ static void drawStatusMsg(u8 joy, u8 line){
 	}
 
 	//draw in either 1p column or 2p column
-	drawText(text, X_STATVAL + ((u16) joy * (W_TILES/2)), Y_STATUS+line, 0, 6);
+
+	//the `- joy` means subtract 1 from x if on 2p column
+	// u8 x = X_STATVAL + (joy * (WIDTH_TILES/2)) - (joy);
+	u8 x = X_STATVAL + leftEdge(joy);
+	drawText(text, x, Y_STATUS+line, 0, 6);
 }
--- src/main.c
+++ src/main.c
@@ -1,31 +1,48 @@
 #include <genesis.h>
+#include "Game.h"
 #include "Instrument.h"
 
-static void joyEvent(u16 joy, u16 changed, u16 state);
+// static void joyEvent(u16 joy, u16 changed, u16 state);
 
-static void joyEvent(u16 joy, u16 changed, u16 state){
-	Instrument_joyEvent(joy, changed, state);
-	HUD_joyEvent(joy, changed, state);
-}
+// static void joyEvent(u16 joy, u16 changed, u16 state){
+// 	Instrument_joyEvent(joy, changed, state);
+// 	HUD_joyEvent(joy, changed, state);
+// }
 
 int main(){
 	
-	JOY_init();
-	JOY_setEventHandler(&joyEvent);
+	JOY_setEventHandler(&Game_joyEvent);
+	JOY_setSupport(PORT_1, JOY_SUPPORT_6BTN);
+	JOY_setSupport(PORT_2, JOY_SUPPORT_6BTN);
 	PSG_init();
-	Instrument_init();
-	HUD_init();
+
+	Game_addInitCallback(&Instrument_init);
+	Game_addInitCallback(&HUD_init);
+
+	Game_addUpdateCallback(&Instrument_update);
+	Game_addUpdateCallback(&HUD_update);
+
+	Game_addPauseCallback(&Instrument_pauseSet);
+	Game_addPauseCallback(&HUD_pauseSet);
+
+	Game_addJoyEventCallback(&Instrument_joyEvent);
+	Game_addJoyEventCallback(&HUD_joyEvent);
+
+	// Instrument_init();
+	// HUD_init();
+
+	Game_init();
 
 	//VDP_setTileMap(APLAN, TILE_ATTR_FULL(PAL1, 1, 1, 1, 1), 5, 5);
 
 	//main loop
 	while(1)
 	{
-		Instrument_update();
-		HUD_update();
-		VDP_waitVSync();
+		// Instrument_update();
+		// HUD_update();
+		Game_update();
 
-		//scan for new controllers
+		VDP_waitVSync();
 	}
 
 	return 0;
